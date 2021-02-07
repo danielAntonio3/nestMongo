8b0dc8560e47aad72e2e2a9d5cd9fcc304c5aac6
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2021-02-06 23:37:12 -0600

    creando proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductoModule } from './producto/producto.module';
+
+// *IMPORTAR MONGOOSE
+// ?PRIMER PASO
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [ProductoModule],
   controllers: [AppController],
   providers: [AppService],
 })
