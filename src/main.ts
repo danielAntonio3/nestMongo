7fc82f6372cec0e43b560ca42c47f566198e11aa
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2021-02-12 15:16:52 -0600

    agregando cosas para que funcione
    Esta actualización contiene la configuración necesaria para evitar el error de crashear
    code=H10 desc=”App crashed” method=GET path=”/”

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //await app.listen(3000);
+  // ! ESTO ES PARA QUE JALE EN HEROKU YA QUE ELLOS NO OCUPAN NUESTRO PUERTO SI NO EL DE ELLOS
+  // ? AGREGAR UN NUEVOR ACHIVO EN RAIZ CON EL NOMBRE DE Procfile
+  // ? web: npm run start:prod
+  // ? EN packge.json AGREGA EN SCRIPT AL FINAL LO SIGUIENTE
+  // * "heroku-postbuild":"npm install --only=dev --no-shrinkwrap && npm run build"
+  // !AGREGA LA SIGUEINTRE LINEA
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
